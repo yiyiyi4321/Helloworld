616a50e296559e1dffc0a392418386b68d7d8726
yiyiyi4321 <18735763@qq.com>
2019-07-09 00:11:05 +0800

    adjust

--- transform_pdf.py
+++ transform_pdf.py
@@ -4,14 +4,14 @@ input_file = open('in.txt', 'r', encoding='utf-8')
 
 output = ""
 for line in input_file:
-    if line.strip()[-1] == '-':
+    if len(line) > 1 and line.strip()[-1] == '-':
         output += line.strip()[:-1]
     else:
         output += line.strip() + ' '
 
 url = 'https://translate.google.com/#view=home&op=translate&sl=auto&tl=zh-CN&text='
 url += output.replace(' ', '%20')
-print(url)
+print(output)
 webbrowser.open(url)
 
 input_file.close()
